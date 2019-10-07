71cca72739bc64ea4171f5437be8d8e52f42bd94
Matthew McCorry <mccorry@gmail.com>
2019-10-07 20:34:20 +0100

    initial commit

---
+++ zipcodes.fsx
@@ -0,0 +1,25 @@
+#r "./packages/FSharp.Data/lib/net45/FSharp.Data.dll"
+open FSharp.Data
+open System
+
+type ZipCodes = HtmlProvider<"https://pe.usps.com/Archive/HTML/DMMArchive20050106/print/L002.htm">
+
+let table = ZipCodes.Load("https://pe.usps.com/Archive/HTML/DMMArchive20050106/print/L002.htm").Tables.``L002 3-Digit ZIP Code Prefix Matrix``
+
+let rec secondToLast ls =
+    match ls with
+    |   []         -> None
+    |   h :: [_]   -> Some(h)
+    |   h :: t     -> secondToLast t
+
+let extractState (s: string) = s.Split (' ') |> Seq.toList |> secondToLast
+
+let toState =
+    extractState >>
+    function
+    | Some a -> a
+    | None -> ""
+
+let rows = table.Rows |> Seq.map (fun r -> r.``3‑Digit ZIP Code Prefix`` + "," + (toState r.``Column A 3‑Digit Destinations Label to``))
+
+System.IO.File.WriteAllLines ("zips.csv", rows)
